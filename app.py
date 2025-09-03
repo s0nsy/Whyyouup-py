8c7aadb934a0d49f8dde7e644391ec69ebafe8d5
sonsy__20 <20200927@sungshin.ac.kr>
2025-09-03 18:00:53 +0900

    Fix: 예측 기능 수정

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+import torch.nn.functional as F
 
 app = Flask(__name__)
 
-model_path = "./finetuned_model"
-sentiment_analyzer = None
+model_path = "./fine_tuned_model"
+tokenizer = BertTokenizer.from_pretrained(model_path)
+model = BertForSequenceClassification.from_pretrained(model_path)
+model.eval()
 
-def load_model():
-    global sentiment_analyzer
-    sentiment_analyzer = pipeline("text-classification", model=model_path, tokenizer=model_path)
+id2label = {
+    0: "highly negative",
+    1: "negative",
+    2: "slightly negative",
+    3: "neutral",
+    4: "slightly positive",
+    5: "positive",
+    6: "highly positive"
+}
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
-    text = data.get('text')
+    text = data.get('text',None)
 
     if not text:
         return jsonify({"error": "No text provided"}), 400
 
-    result = sentiment_analyzer(text, truncation=True, max_length=256)[0]
+    inputs = tokenizer(text, return_tensors="pt", truncation= True, padding= True, max_length=1024)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+
+        probs = F.softmax(logits, dim=1)
+        print(probs)  # 여기서 각 클래스 확률 확인 가능
+
+        predicted_class = torch.argmax(logits, dim = 1).item()
+        predicted_label = id2label[predicted_class]
 
     return jsonify({
-        "label": result['label'],  # e.g., "LABEL_0", "LABEL_1", "LABEL_2"
-        "score": float(result['score'])
+        "text": text,
+        "predicted_class": predicted_class,
+        "predicted_label": predicted_label
     })
 
-@app.route('/reload', methods=['POST'])
-def reload_model():
-    load_model()
-    return jsonify({"status": "reloaded"})
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port = 5000, debug = True)
 
 
